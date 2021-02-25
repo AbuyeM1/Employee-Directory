b54147095a8b5d5ddd136b576be866d98d297cc3
Abuye Mamuye <abuyeye3@gmail.com>
2021-02-24 16:38:32 -0800

    add code

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React,  {Component, components} from 'react';
+import employees from './employees.json';
 import './App.css';
+import EmployeeRow from './components/EmployeeRow/index.js';
+import Footer from './components/Footer/index.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { employees: employees.sort((a, b) => (a.last_name.toLowerCase() > b.last_name.toLowerCase()) ? 1 : -1), 
+    ascending: true }
+
+  //This function allows users to filter by last name//
+  handleChange = event => {
+    let userInput = event.target.value;
+      if(userInput === "") {
+        this.setState({employees: employees.sort((a, b) => (a.last_name.toLowerCase() > b.last_name.toLowerCase()) ? 1 : -1)})
+      } else {
+        const characters = employees;
+
+    const result = characters.filter(employee => employee.last_name.toLowerCase().match(userInput.toLowerCase()) !== null);
+  
+    this.setState({employees: result})} 
+  }
+
+  //This function allows users to sort on multiple column titles.//
+  handleSort = (columnTitle) => {
+    let sortArray;
+    const toggle = !this.state.ascending;
+    if(toggle) {
+      sortArray = employees.sort((a, b) => (a[columnTitle].toLowerCase() > b[columnTitle].toLowerCase()) ? 1 : -1)
+    } else {
+      sortArray = employees.sort((a, b) => (b[columnTitle].toLowerCase() > a[columnTitle].toLowerCase()) ? 1 : -1)
+    }
+    this.setState( { employees: sortArray, ascending: toggle } );
+  }
+
+  
 }
+
 
 export default App;
---
+++ employee-directory/src/Footer/index.js
@@ -0,0 +1,12 @@
+import Reaact, {Component} from 'react';
+import './style.css';
+
+class Footer extends Component {
+    render() {
+        return (
+        <div className="copyright">copyright &copy: 2020 </div>
+        );
+    }
+}
+ 
+export default Footer;
